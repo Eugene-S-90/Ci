2100bf0633adea2507a41a0eaad0febbb0d84fef
Eugene Sokolianskyi <eugenesokol04@gmail.com>
2025-05-13 12:52:03 +0300

    test_push_3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1 className="text-4xl font-bold mb-8">Welcome to Next.js</h1>
-        <h2 className="text-2xl font-bold mb-8">Hello!</h2>
+        <h2 className="text-2xl font-bold mb-8">Hello! again</h2>
       </div>
     </main>
   )
